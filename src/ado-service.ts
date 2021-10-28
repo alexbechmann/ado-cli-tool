e4977576875fc6b37a38654ec3c57618279f550a
Alex <alex.bechmann@outlook.com>
2021-10-29 00:27:26 +0200

    refactor to use azure-devops-node-api

--- src/ado-service.ts
+++ src/ado-service.ts
@@ -1,34 +1,15 @@
-import axios from "axios";
 import * as storageService from "./storage-service";
 import keytar from "keytar";
+import * as azdev from "azure-devops-node-api";
 
-const client = axios.create({
-  baseURL: `https://dev.azure.com/${
-    storageService.get().azureDevopsOrganization
-  }`,
-  headers: {},
-});
-
-console.log(
-  `https://dev.azure.com/${storageService.get().azureDevopsOrganization}`
-);
-
-client.interceptors.request.use(async (requestConfig) => {
+async function createConnection() {
   const pat = await getToken();
-  const patBase64 = Buffer.from(`:${pat}`).toString("base64");
-  requestConfig.headers["Authorization"] = `Basic ${patBase64}`;
-  return requestConfig;
-});
-
-client.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    console.log(error.response.statusText, error.response.data);
-    return error.response;
-  }
-);
+  const authHandler = azdev.getPersonalAccessTokenHandler(pat);
+  const { azureDevopsOrganization } = storageService.get();
+  const orgUrl = `https://dev.azure.com/${azureDevopsOrganization}`;
+  const connection = new azdev.WebApi(orgUrl, authHandler);
+  return connection;
+}
 
 export async function storeToken(token: string) {
   await keytar.setPassword("ado-cli-tool", "default", token);
@@ -38,22 +19,22 @@ export function getToken() {
   return keytar.getPassword("ado-cli-tool", "default");
 }
 
-export async function getProjects(): Promise<any[]> {
+export async function getProjects() {
   console.log(`Getting projects...`);
-  const response = await client.get("/_apis/projects");
-  const projects = response.data.value.sort((a, b) =>
+  const connection = await createConnection();
+  const core = await connection.getCoreApi();
+  const projects = await core.getProjects();
+  return projects.sort((a, b) =>
     a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
   );
-  return projects;
 }
 
 export async function getRepos(projectId: string): Promise<any[]> {
   console.log(`Getting repos...`);
-  const response = await client.get(
-    `/${projectId}/_apis/git/repositories?api-version=6.1-preview.1`
-  );
-  const repos = response.data.value.sort((a, b) =>
+  const connection = await createConnection();
+  const git = await connection.getGitApi();
+  const repos = await git.getRepositories(projectId);
+  return repos.sort((a, b) =>
     a.name.localeCompare(b.name, undefined, { sensitivity: "base" })
   );
-  return repos;
 }
